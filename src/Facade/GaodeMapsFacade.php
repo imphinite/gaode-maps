9d357c3cf7ff7a26282060273b3b2999a2f0ed80
Charles Wang <charles.w.developer@gmail.com>
2017-12-18 13:04:40 -0800

    fix bootstrap

---
+++ src/Facade/GaodeMapsFacade.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace GaodeMaps;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \Illuminate\Html\HtmlBuilder
+ */
+class GaodeMapsFacade extends Facade 
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() 
+    {
+         return 'GoogleMaps';
+    }
+}
---
+++ src/ServiceProvider/GaodeMapsServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace GaodeMaps;
+
+use Illuminate\Support\ServiceProvider;
+
+class GaodeMapsServiceProvider extends ServiceProvider
+{
+    /**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = true;
+    
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+                __DIR__.'/../config/gaodemaps.php' => config_path('gaodemaps.php'),
+            ], 'gaodemaps');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('GaodeMaps', function($app)
+        {
+            return new \GaodeMaps\GaodeMaps();
+        });
+    }
+        
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('GaodeMaps');
+    }
+}
